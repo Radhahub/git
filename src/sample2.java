4bec557e168f717b5d01672e3a008a2c60dd60ad
Radhahub <radhayadavrr03@gmail.com>
2022-01-28 14:04:22 +0530

    sample 2 automated

--- src/sample2.java
+++ src/sample2.java
@@ -1,7 +1,9 @@
 
 public class sample2
 {
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
 	System.out.println("this is my second java class");
+	System.out.println("this is my java class");
 }
 }
