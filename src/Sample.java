89a14a135884ffcca0452f804dce3d6d85fe9530
Radhahub <radhayadavrr03@gmail.com>
2022-01-28 13:54:00 +0530

    sample 2 automated

--- src/Sample.java
+++ src/Sample.java
@@ -4,5 +4,6 @@ public class Sample
   public static void main(String[] args)
   {
 	System.out.println("this is my first git bash");
+	System.out.println("this is my java class");
 }
 }
---
+++ src/sample2.java
@@ -0,0 +1,7 @@
+
+public class sample2
+{
+  public static void main(String[] args) {
+	System.out.println("this is my second java class");
+}
+}
